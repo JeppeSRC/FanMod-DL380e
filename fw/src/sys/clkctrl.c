e89b05981a06e35800e40fa98cc367bdac86a37f
unknown <theholyhorse.dev@gmail.com>
2021-03-20 08:56:30 +0100

    Whoopsie forgottie thesei

--- fw/src/sys/clkctrl.c
+++ fw/src/sys/clkctrl.c
@@ -2,10 +2,10 @@
 #include "sys.h"
 
 void  clkctrl_write(uint8 registerOffset, uint16 bits, uint8 data) {
-	write(CLKCTRL_ADDR + registerOffset, bits, data);
+    write(CLKCTRL_ADDR + registerOffset, bits, data);
 }
 
 uint8 clkctrl_read(uint8 registerOffset, uint16 bits) {
-	return read(CLKCTRL_ADDR + registerOffset, bits);
+    return read(CLKCTRL_ADDR + registerOffset, bits);
 }
 
--- fw/src/sys/sys.c
+++ fw/src/sys/sys.c
@@ -2,16 +2,16 @@
 
 
 void write(uint16 registerAddress, uint16 bits, uint8 data) {
-	volatile uint8* reg = (uint8*)registerAddress;
+    volatile uint8* reg = (uint8*)registerAddress;
 
-	uint8 tmp = *reg & ~MASK(bits);
-	tmp |= (data << SHIFT(bits)) & MASK(bits);
-	*reg = tmp;
+    uint8 tmp = *reg & ~MASK(bits);
+    tmp |= (data << SHIFT(bits)) & MASK(bits);
+    *reg = tmp;
 }
 
 uint8 read(uint16 registerAddress, uint16 bits) {
-	volatile uint8* reg = (uint8*)registerAddress;
+    volatile uint8* reg = (uint8*)registerAddress;
 
-	return (*reg >> SHIFT(bits)) & MASK(bits);
+    return (*reg >> SHIFT(bits)) & MASK(bits);
 }
 
