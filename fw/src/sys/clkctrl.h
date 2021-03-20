ac73ccc550d23a6af47d498252e70e50dc3b290b
unknown <theholyhorse.dev@gmail.com>
2021-03-20 09:25:07 +0100

    Prefixes

--- fw/src/sys/clkctrl.h
+++ fw/src/sys/clkctrl.h
@@ -3,7 +3,7 @@
 
 #include "def.h"
 
-#define CLKCTRL_ADDR 0x0060
+#define CLKCTRL_ADDR            0x0060
 
 #define CLKCTRL_MCLKCTRLA       0x00
 #define CLKCTRL_MCLKCTRLB       0x01
@@ -16,30 +16,30 @@
 
 
 //MCLKCTRLA
-#define CLKSEL                  MAKEBITS(0x03, 0)
+#define CLKCTRL_CLKSEL          MAKEBITS(0x03, 0)
 
 //MCLKCTRLB
-#define PEN                     MAKEBITS(0x01, 0)
-#define PDIV                    MAKEBITS(0x0F, 1)
+#define CLKCTRL_PEN             MAKEBITS(0x01, 0)
+#define CLKCTRL_PDIV            MAKEBITS(0x0F, 1)
 
 //MCLKLOCK
-#define LOCKEN                  MAKEBITS(0x01, 0)
+#define CLKCTRL_LOCKEN          MAKEBITS(0x01, 0)
 
 //MCLKSTATUS
-#define SOSC                    MAKEBITS(0x01, 0)
-#define OSC20MS                 MAKEBITS(0x01, 4)
-#define OSC32KS                 MAKEBITS(0x01, 5)
-#define EXTS                    MAKEBITS(0x01, 7)
+#define CLKCTRL_SOSC            MAKEBITS(0x01, 0)
+#define CLKCTRL_OSC20MS         MAKEBITS(0x01, 4)
+#define CLKCTRL_OSC32KS         MAKEBITS(0x01, 5)
+#define CLKCTRL_EXTS            MAKEBITS(0x01, 7)
 
 //OSC20MCTRLA
-#define RUNSTDBY                MAKEBITS(0x01, 1)
+#define CLKCTRL_RUNSTDBY        MAKEBITS(0x01, 1)
 
 //OSC20MCALIBA
-#define CAL20M                  MAKEBITS(0x3F, 0)
+#define CLKCTRL_CAL20M          MAKEBITS(0x3F, 0)
 
 //OSC20MCALIBB
-#define TEMPCAL20M              MAKEBITS(0x0F, 0)
-#define LOCK                    MAKEBITS(0x01, 7)
+#define CLKCTRL_TEMPCAL20M      MAKEBITS(0x0F, 0)
+#define CLKCTRL_LOCK            MAKEBITS(0x01, 7)
 
 //OSC32KCTRLA
 //#define RUNSTDBY
--- fw/src/sys/cpuint.h
+++ fw/src/sys/cpuint.h
@@ -3,28 +3,28 @@
 
 #include "sys.h"
 
-#define CPUINT_ADDR 0x0110
+#define CPUINT_ADDR         0x0110
 
-#define CPUINT_CTRLA    0x00
-#define CPUINT_STATUS   0x01
-#define CPUINT_LVL0PRI  0x02
-#define CPUINT_LCL1VEC  0x03
+#define CPUINT_CTRLA        0x00
+#define CPUINT_STATUS       0x01
+#define CPUINT_LVL0PRI      0x02
+#define CPUINT_LCL1VEC      0x03
 
 //CTRLA
-#define LVL0RR          MAKEBITS(0x01, 0)
-#define CVT             MAKEBITS(0x01, 5)
-#define IVSEL           MAKEBITS(0x01, 6)
+#define CPUINT_LVL0RR       MAKEBITS(0x01, 0)
+#define CPUINT_CVT          MAKEBITS(0x01, 5)
+#define CPUINT_IVSEL        MAKEBITS(0x01, 6)
 
 //STATUS
-#define	LVL0EX          MAKEBITS(0x01, 0)
-#define LVL1EX          MAKEBITS(0x01, 1)
-#define NMIEX           MAKEBITS(0x01, 7)
+#define	CPUINT_LVL0EX       MAKEBITS(0x01, 0)
+#define CPUINT_LVL1EX       MAKEBITS(0x01, 1)
+#define CPUINT_NMIEX        MAKEBITS(0x01, 7)
 
 //LVL0PRI
-#define LVL0PRI         MAKEBITS(0xFF, 0)
+#define CPUINT_LVL0PRI      MAKEBITS(0xFF, 0)
 
 //LVL1VEC
-#define LVL1VEC         MAKEBITS(0xFF, 0)
+#define CPUINT_LVL1VEC      MAKEBITS(0xFF, 0)
 
 void  cpuint_write(uint8 registerOffset, uint16 bits, uint8 data);
 uint8 cpuint_read(uint8 registerOffset, uint16 bits);
