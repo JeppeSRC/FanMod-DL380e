75156cd40fef3ebba20efc0b16d4329b608dfcf3
unknown <theholyhorse.dev@gmail.com>
2021-03-20 08:54:11 +0100

    Replaced tabs with spaces

--- fw/src/sys/cpuint.c
+++ fw/src/sys/cpuint.c
@@ -1,9 +1,9 @@
 #include "cpuint.h"
 
 void  cpuint_write(uint8 registerOffset, uint16 bits, uint8 data) {
-	write(CPUINT_ADDR + registerOffset, bits, data);
+    write(CPUINT_ADDR + registerOffset, bits, data);
 }
 
 uint8 cpuint_read(uint8 registerOffset, uint16 bits) {
-	return read(CPUINT_ADDR + registerOffset, bits);
+    return read(CPUINT_ADDR + registerOffset, bits);
 }
