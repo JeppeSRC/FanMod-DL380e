7fe20e747e13b49967e62e068e9f09773afe847b
unknown <theholyhorse.dev@gmail.com>
2021-03-21 08:28:40 +0100

    Added some math stuff

---
+++ fw/src/math/math.c
@@ -0,0 +1,29 @@
+#include "math.h"
+
+uint8 log2(uint8 v) {
+    switch (v) {
+        case 0x80:
+            return 7;
+        case 0x40:
+            return 6;
+        case 0x20:
+            return 5;
+        case 0x10:
+            return 4;
+        case 0x08:
+            return 3;
+        case 0x04:
+            return 2;
+        case 0x02:
+            return 1;
+        case 0x01:
+            return 0;
+    }
+
+    return 0xFF;
+}
+
+uint8 maskLSB(uint8 v) {
+    if (v == 0xFF) return 0;
+    return v &= ~v + 1;
+}
---
+++ fw/src/math/math.h
@@ -0,0 +1,9 @@
+#ifndef _LOG_H_
+#define _LOG_H_
+
+#include <sys/def.h>
+
+uint8 log2(uint8 v);
+uint8 maskLSB(uint8 v);
+
+#endif
