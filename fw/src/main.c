e11076e52b4662e4b43bbd892260500d7a481471
unknown <theholyhorse.dev@gmail.com>
2021-03-23 18:50:40 +0100

    Frequency measurements implemented

--- fw/src/main.c
+++ fw/src/main.c
@@ -1,8 +1,10 @@
 #include <sys/cpuint.h>
 #include <sys/clkctrl.h>
 #include <sys/tca0.h>
+#include <sys/tcb0.h>
 #include <sys/twi.h>
 #include <sys/port.h>
+#include <sys/evsys.h>
 
 #define CMD_GET_SET_PWM 0x01
 #define CMD_SET_PWM 0x02
@@ -50,6 +52,15 @@ void TWIS_Handler() {
     }
 }
 
+void TCB_Handler() {
+    uint16 cnt = tcb_readw(TCB_CNT);
+    uint16 ccmp = tcb_readw(TCB_CCMP);
+
+    float duty = (float)ccmp / (float)cnt;
+
+    requestedPWM = (uint8)(200.0f * duty);
+}
+
 uint8 currentPWM = 0;
 
 void init() {
@@ -63,7 +74,14 @@ void init() {
     tca_writew(TCA_CMP0, 400); //Default to 50% duty cycle
     tca_write(TCA_CTRLA, TCA_bENABLE, 1); //Enable counter
 
+    port_pin_mode(6, 0); //Set PA6 to input
 
+    evsys_write(EVSYS_ASYNCCH0, EVSYS_bASYNCHCH, 0x10); //Set PA6 as event source for ASYNCCH0
+    evsys_write(EVSYS_ASYNCUSER0, EVSYS_bASYNCUSER, 0x03); //Set ASYNCH0 as source for TCB input events
+
+    tcb_write(TCB_CTRLB, 0xFF, 0x05); // Enable frequency and pulse-width measurements
+    tcb_write(TCB_INTCTRL, 0xFF, 0x01); // Enable capture interrupt
+    tcb_write(TCB_CTRLA, 0xFF, 0x01); // Enable
 }
 
 void main() {
